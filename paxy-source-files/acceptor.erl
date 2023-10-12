6129a33d17b97d10988a1c2fc4f88e8e66e76974
Vincent A. Arcila <vincent.arcila@bsc.es>
2023-10-12 14:57:45 +0200

    Finished 2.2

--- paxy-source-files/acceptor.erl
+++ paxy-source-files/acceptor.erl
@@ -1,8 +1,6 @@
 -module(acceptor).
 -export([start/2]).
 
--define(delay, 500).
-
 start(Name, PanelId) ->
     spawn(fun() -> init(Name, PanelId) end).
 
@@ -21,8 +19,9 @@ acceptor(Name, Promised, Voted, Value, PanelId) ->
                     %% promise this value because the message ID (Round) is greater
                     %% than the last promise made
                     %% Proposer ! {promise, Round, Voted, Value},
-                    T = rand:uniform(?delay),
-                    timer:send_after(T, Proposer, {promise, Round, Voted, Value}),
+                    %T = rand:uniform(?delay),
+                    %timer:send_after(T, Proposer, {promise, Round, Voted, Value}),
+                    Proposer ! {promise, Round, Voted, Value},
                     io:format(
                         "[Acceptor ~w] Phase 1: promised ~w voted ~w colour ~w~n",
                         [Name, Round, Voted, Value]
@@ -38,7 +37,7 @@ acceptor(Name, Promised, Voted, Value, PanelId) ->
                             "Promised: " ++ io_lib:format("~p", [Promised]), Colour},
                     acceptor(Name, Round, Voted, Value, PanelId);
                 false ->
-                    Proposer ! {sorry, {prepare, Round}},
+                    % Proposer ! {sorry, {prepare, Round}},
                     %% T = rand:uniform(?delay),
                     %% timer:send_after(T, Proposer, {sorry, {prepare, Round}}),
                     acceptor(Name, Promised, Voted, Value, PanelId)
@@ -49,8 +48,7 @@ acceptor(Name, Promised, Voted, Value, PanelId) ->
                 true ->
                     %% !!!! He cambiado Proposal por Round. Lo que se tiene que devolver es un identificador, no un color
                     %% Proposer ! {vote, Round},
-                    T = rand:uniform(?delay),
-                    timer:send_after(T, Proposer, {vote, Round}),
+                    Proposer ! {vote, Round},
                     %% M-C: Estoy de acuerdo
                     %% Context: Es el number que me han dado mayor o igual que el que he votado?
                     case order:goe(Round, Voted) of
@@ -71,7 +69,7 @@ acceptor(Name, Promised, Voted, Value, PanelId) ->
                     end;
                 false ->
                     %% Context: Siempre en caso de tener numero menor que prometido, devolver sorry
-                    Proposer ! {sorry, {accept, Promised}},
+                    % Proposer ! {sorry, {accept, Promised}},
                     acceptor(Name, Promised, Voted, Value, PanelId)
             end;
         stop ->
