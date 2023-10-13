dfd037ded9c23bd476389f2168b238e39e4a3824
Vincent Arcila <jandro.vins@gmail.com>
2023-10-13 12:17:45 +0200

    Added run scripts

---
+++ paxy-source-files/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+not_sourced() {
+    >&2 echo "ERROR: Script should be sourced"
+    exit 1
+}
+
+(return 0 2>/dev/null) || not_sourced
+
+
+numlist="$(python gen10rand.py)"
+erl -pa ebin -eval "paxy:start($numlist)"
+
