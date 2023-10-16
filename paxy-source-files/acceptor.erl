fb667d526cbfd4a095dfb570574fe13473696d3c
santo0 <lemarti3472@gmail.com>
2023-10-16 22:48:51 +0200

    comments

--- paxy-source-files/acceptor.erl
+++ paxy-source-files/acceptor.erl
@@ -18,9 +18,6 @@ acceptor(Name, Promised, Voted, Value, PanelId) ->
                     %% Phase 1b: We have received a "prepare" message, and we can
                     %% promise this value because the message ID (Round) is greater
                     %% than the last promise made
-                    %% Proposer ! {promise, Round, Voted, Value},
-                    %T = rand:uniform(?delay),
-                    %timer:send_after(T, Proposer, {promise, Round, Voted, Value}),
                     Proposer ! {promise, Round, Voted, Value},
                     io:format(
                         "[Acceptor ~w] Phase 1: promised ~w voted ~w colour ~w~n",
@@ -38,19 +35,14 @@ acceptor(Name, Promised, Voted, Value, PanelId) ->
                     acceptor(Name, Round, Voted, Value, PanelId);
                 false ->
                     % Proposer ! {sorry, {prepare, Round}},
-                    %% T = rand:uniform(?delay),
-                    %% timer:send_after(T, Proposer, {sorry, {prepare, Round}}),
                     acceptor(Name, Promised, Voted, Value, PanelId)
             end;
         {accept, Proposer, Round, Proposal} ->
-            %% Context: Acceptor prometió rechazar prepare/accept con menor number
+            %% Context: Acceptor promised to decline smaller Round accepts
             case order:goe(Round, Promised) of
                 true ->
-                    %% !!!! He cambiado Proposal por Round. Lo que se tiene que devolver es un identificador, no un color
-                    %% Proposer ! {vote, Round},
                     Proposer ! {vote, Round},
-                    %% M-C: Estoy de acuerdo
-                    %% Context: Es el number que me han dado mayor o igual que el que he votado?
+                    %% Is the given Round bigger or equal than my Voted?
                     case order:goe(Round, Voted) of
                         true ->
                             io:format(
@@ -61,14 +53,14 @@ acceptor(Name, Promised, Voted, Value, PanelId) ->
                             PanelId !
                                 {updateAcc, "Voted: " ++ io_lib:format("~p", [Round]),
                                     "Promised: " ++ io_lib:format("~p", [Promised]), Proposal},
-                            %% Round pasa a ser el voted
+                            %% Proposer's Round is voted
                             acceptor(Name, Promised, Round, Proposal, PanelId);
                         false ->
-                            %% Voted queda invicto, no devuelve sorry o similar en este caso
+                            %% My Voted wins and stays the same
                             acceptor(Name, Promised, Voted, Value, PanelId)
                     end;
                 false ->
-                    %% Context: Siempre en caso de tener numero menor que prometido, devolver sorry
+                    %% Context: In case of Round smaller than Promised, return sorry
                     % Proposer ! {sorry, {accept, Promised}},
                     acceptor(Name, Promised, Voted, Value, PanelId)
             end;
