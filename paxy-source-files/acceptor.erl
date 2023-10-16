59dbe4de6fcf35e5bb5ccf365b0807fbd1590533
Daniel Trujillo Viedma <dtviedma@gmail.com>
2023-10-16 23:14:27 +0200

    Fix comments and remove unnecessary send_after

--- paxy-source-files/acceptor.erl
+++ paxy-source-files/acceptor.erl
@@ -20,9 +20,7 @@ acceptor(Name, Promised, Voted, Value, PanelId) ->
                     %% Phase 1b: We have received a "prepare" message, and we can
                     %% promise this value because the message ID (Round) is greater
                     %% than the last promise made
-                    %% Proposer ! {promise, Round, Voted, Value},
-                    T = rand:uniform(?delay),
-                    timer:send_after(T, Proposer, {promise, Round, Voted, Value}),
+                    Proposer ! {promise, Round, Voted, Value},
                     io:format(
                         "[Acceptor ~w] Phase 1: promised ~w voted ~w colour ~w~n",
                         [Name, Round, Voted, Value]
@@ -39,20 +37,14 @@ acceptor(Name, Promised, Voted, Value, PanelId) ->
                     acceptor(Name, Round, Voted, Value, PanelId);
                 false ->
                     Proposer ! {sorry, {prepare, Round}},
-                    %% T = rand:uniform(?delay),
-                    %% timer:send_after(T, Proposer, {sorry, {prepare, Round}}),
                     acceptor(Name, Promised, Voted, Value, PanelId)
             end;
         {accept, Proposer, Round, Proposal} ->
-            %% Context: Acceptor prometió rechazar prepare/accept con menor number
+            %% Context: Acceptor promised to decline smaller Round accepts
             case order:goe(Round, Promised) of
                 true ->
-                    %% !!!! He cambiado Proposal por Round. Lo que se tiene que devolver es un identificador, no un color
-                    %% Proposer ! {vote, Round},
-                    T = rand:uniform(?delay),
-                    timer:send_after(T, Proposer, {vote, Round}),
-                    %% M-C: Estoy de acuerdo
-                    %% Context: Es el number que me han dado mayor o igual que el que he votado?
+                    Proposer ! {vote, Round},
+                    %% Is the given Round bigger or equal than my Voted?
                     case order:goe(Round, Voted) of
                         true ->
                             io:format(
@@ -63,14 +55,14 @@ acceptor(Name, Promised, Voted, Value, PanelId) ->
                             PanelId !
                                 {updateAcc, "Voted: " ++ io_lib:format("~p", [Round]),
                                     "Promised: " ++ io_lib:format("~p", [Promised]), Proposal},
-                            %% Round pasa a ser el voted
+                            %% Proposer's Rounds is voted
                             acceptor(Name, Promised, Round, Proposal, PanelId);
                         false ->
-                            %% Voted queda invicto, no devuelve sorry o similar en este caso
+                            %% My Voted wins and stays the same
                             acceptor(Name, Promised, Voted, Value, PanelId)
                     end;
                 false ->
-                    %% Context: Siempre en caso de tener numero menor que prometido, devolver sorry
+                    %% Context: In case of Round smaller than Promised, return sorry
                     Proposer ! {sorry, {accept, Promised}},
                     acceptor(Name, Promised, Voted, Value, PanelId)
             end;
